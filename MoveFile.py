8fa72ac107b13111076d4d8afcc0888d29babf4c
calle <callerin@live.com>
2021-05-16 18:32:29 +0800

    finally

--- MoveFile.py
+++ MoveFile.py
@@ -6,11 +6,13 @@
 import os
 import re
 import sys
+import time
 
 from shutil import move
 from send2trash import send2trash
 
 file_remove = []
+count = 0
 
 
 def move_file(origin: str, destination: list, filetype: tuple = ('.mp4', '.jpg', '.nfo'), send_trash=True):
@@ -22,49 +24,60 @@ def move_file(origin: str, destination: list, filetype: tuple = ('.mp4', '.jpg',
 			destination ([type]):   	目标文件夹列表
 			filetype (tuple, optional): 文件后缀. defaults to ('.mp4', '.jpg', '.nfo').
 	"""
-
-	count = 0
+	global count
+	global file_remove
+	
 	file_downloading = []
 	file_moved = []
 	file_exists = []
-
+	file_remove = []
+	
 	del_name = ['情报', '有趣', '直播', '魔王', '地址', '推荐', '.url', 'png', 'txt', 'mht', 'gif', 'nfo']
-
+	
 	for root, dirs, files in os.walk(origin):
 		for file in files:
 			if file + '.aria2' in files or file.endswith('.aria2'):
 				file_downloading.append(file)
-				break
-
+				continue
+			
 			file_t = file_type(file)
 			try:
-				filep = destination[file_t]
+				file_destination = destination[file_t]
 			except Exception as e:
 				print(e)
 				continue
-
+			
 			file_src = os.path.join(root, file)
-			file_des = os.path.join(filep, file_src.split('\\')[-1])
-
+			file_des = os.path.join(file_destination, file_src.split('\\')[-1])
+			
 			if send_trash and any(name in file for name in del_name):
 				file_remove.append(file)
-				send2trash(file_src)
+				try:
+					send2trash(file_src)
+				except Exception as e:
+					print(e)
+					os.remove(file_src)
 				continue
-
+			
 			if file_src.endswith(filetype):
 				if os.path.exists(file_des):
 					file_exists.append(file_des)
 					continue
-
-				file_src = rename_file(file_src)
-
-				move(file_src, file_des)
-				file_moved.append(file_src.split('\\')[-1] + ' is moved to ' + filep.split('\\')[-1])
-
+				
+				# file_src = rename_file(file_src)
+				des_split = file_destination.split('\\')[-1]
+				print(f'{file} is moving to {des_split}')
+				
+				try:
+					move(file_src, file_des)
+				except Exception as e:
+					print(e)
+					os.remove(os.path.join(file, file_des))
+				
+				file_moved.append(file_src.split('\\')[-1] + ' is moved to ' + des_split)
+				
 				count += 1
-
-	print('\nMoved {0} files\n'.format(count))
-
+	
 	my_print(file_moved, '')
 	my_print(file_downloading, 'is downloading')
 	my_print(file_exists, 'exist')
@@ -89,7 +102,7 @@ def rename_file(file: str) -> str:
 		'c.': '3.',
 		'd.': '4.',
 	}
-
+	
 	result = file
 	for pat1 in pattern1:
 		for pat2 in pattern2:
@@ -102,7 +115,7 @@ def rename_file(file: str) -> str:
 				os.rename(file, result)
 				print("{} renamed {}".format(file.split('\\')[-1], result.split('\\')[-1]))
 				break
-
+	
 	return result
 
 
@@ -115,7 +128,8 @@ def remove_null_dirs(origin_dir: str) -> None:
 	Returns:
 
 	"""
-
+	global file_remove
+	
 	for root, dirs, files in os.walk(origin_dir, topdown=False):  # topdown=False 递归文件夹深度 由下到上
 		for dir1 in dirs:
 			dir_path = os.path.join(root, dir1)
@@ -139,10 +153,11 @@ def file_type(filename: str) -> int:
 		1   single
 
 	"""
-	pat = r'\d{2}\.\d{2}\.\d{2}'
+	pat = r'\d{2}\.\d{2}\.\d{2}|[4|2]k|2160|1080'
 	data = re.search(pat, filename)
-
+	
 	if data:
+		# print(data.group())
 		return 1
 	else:
 		return 0
@@ -161,15 +176,40 @@ def my_print(files: list, ending: str):
 	if not files:
 		return
 	print(80 * '-')
+	print()
 	for file in files:
-		print(f'{file:50}', ending)
+		print(f'{file:60}', ending)
+
+
+# Todo add the method that write the change to file
+def write_change(moved: dict):
+	pass
+
+
+def run_period(ori: str, des: list, minutes: float, t: int) -> None:
+	"""
+
+	Args:
+		ori (object):    文件路径
+		des:    目标路径
+		minutes:    间隔时间
+		t:          运行次数
+
+	Returns:
+
+	"""
+	for i in range(t):
+		move_file(ori, des, filetype=('.mp4', '.mkv'))
+		remove_null_dirs(ori)
+		print(f'\n{time.strftime("%b-%d %A %H:%M:%S")}  Running {i + 1} time')
+		time.sleep(int(minutes * 60))
 
 
 if __name__ == '__main__':
 	ori = r'D:\Download\aria2'
 	des = [r'D:\Download\QQDownload\Single', r'D:\Download\EU']
 	file_end = ('.mp4', '.jpg', '.wmv', '.mov', '.mkv', 'avi')
-
+	
 	if len(sys.argv) == 2:
 		if os.path.isdir(sys.argv[1]):
 			ori = sys.argv[1]
@@ -182,7 +222,13 @@ if __name__ == '__main__':
 	for item in des:
 		if not os.path.exists(item):
 			os.makedirs(item)
-
+	
+	print(time.strftime("%b-%d %A %H:%M:%S") + '\n')
 	move_file(ori, des, file_end)
 	remove_null_dirs(ori)
-	move_file('R:\\', des, filetype=('.mp4', '.mkv'), send_trash=False)
+	
+	aria_2 = 'R:\\aria2'
+	
+	move_file(aria_2, des, filetype=('.mp4', '.mkv'))
+	print('\nMoved {0} files\n'.format(count))
+	run_period(aria_2, des, 3, 50)
